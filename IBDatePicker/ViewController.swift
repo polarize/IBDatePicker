d459a34706085b621b311724eaa1809177d6d4db
Issam Bendaas <issam.bendaas@unister.de>
2016-05-23 09:58:13 +0200

    refactor

--- IBDatePicker/ViewController.swift
+++ IBDatePicker/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
 		super.viewWillAppear(animated)
 		datePicker.minimumDate = NSDate().dateByAddingTimeInterval(validPast)
 		datePicker.maximumDate = NSDate()
-		datePicker.delegate = self
+		
 	}
 	
 }
